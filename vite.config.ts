fa324ebf0657bb3c55aa168abd2fbb0a6e295874
caselestat <themismeasure@gmail.com>
2025-06-21 21:14:31 +0200

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/mantra-health-reminder/',
   server: {
     port: 5174,
   },
